16c0c37373c97b3522e36d2835656623f4ec2010
MaxGitt <maxime.j@codeur.online>
2018-09-17 14:41:57 +0200

    template inscription

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -2,10 +2,11 @@
 
 namespace App\Form;
 
-use App\Entity\Nom;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationType extends AbstractType
 {
@@ -14,15 +15,15 @@ class RegistrationType extends AbstractType
         $builder
             ->add('email')
             ->add('username')
-            ->add('password')
-            ->add('confirm_password')
+            ->add('password', PasswordType::class)
+            ->add('confirm_password', PasswordType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Nom::class,
+            'data_class' => User::class,
         ]);
     }
 }
